df76d77ee8c49605b7b6e99640f9deb591919734
Daniel M.T. <danielmoralesteatino2004@gmail.com>
2024-08-30 20:28:22 -0500

    Interfaz login

--- app/src/main/java/com/example/redsocial/registro.kt
+++ app/src/main/java/com/example/redsocial/registro.kt
@@ -2,24 +2,24 @@ package com.example.redsocial
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registro)
 
-        val button = findViewById<Button>(R.id.registro)
+
+        val button = findViewById<Button>(R.id.volver)
 
         button.setOnClickListener{
-            val intent = Intent(this,registro::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
